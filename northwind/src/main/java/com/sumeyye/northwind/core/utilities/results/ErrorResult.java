6483c2d33d82b6ad0708175d4bd5b1e58aa1f723
sumeyyekaratekin <sumeyyekaratekin96@gmail.com>
2021-05-23 00:35:59 +0300

    Swagger added.

---
+++ northwind/src/main/java/com/sumeyye/northwind/core/utilities/results/ErrorResult.java
@@ -0,0 +1,12 @@
+package com.sumeyye.northwind.core.utilities.results;
+
+public class ErrorResult extends Result{
+
+	public ErrorResult() {
+		super(false);
+	}
+	
+	public ErrorResult(String message) {
+		super(false,message);
+	}
+}
